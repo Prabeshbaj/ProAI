f9ddb1d91cd4d401e368bf2aded6487b05ab80d7
Prabesh Buzz <prabeshbuzz01@gmail.com>
2022-12-26 15:24:07 -0500

    updating server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://proai-xb8y.onrender.com', {
     method:'POST',
     headers: {
       'Content-Type': 'application/json'
